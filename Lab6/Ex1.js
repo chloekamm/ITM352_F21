c87d52be237f5c61ade3232ddfcb6e6f42f98025
chloekamm <kamchloe11@gmail.com>
2021-09-27 11:38:48 -1000

    Lab6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,10 @@
+var age =  19;
+var fav_num = 4;
+var day_of_birth = 5;
+var month_of_birth = 10;
+
+console.log(age + fav_num / day_of_birth * month_of_birth
+    );
+
+    console.log((age + fav_num) / day_of_birth * month_of_birth
+        );
