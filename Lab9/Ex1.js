0f4698a77d69a728b87e90d8da7a763d9ba78186
chloekamm <kamchloe11@gmail.com>
2021-10-31 11:14:13 -1000

    change

---
+++ Lab9/Ex1.js
@@ -0,0 +1,18 @@
+var month = 8;
+var day = 3;
+var year = 2013;
+
+
+step1 = 13;
+step2 = parseInt(step1/4);
+step3 = step1 + step2;
+step4 = 2;
+
+step6 = step4 + step3;
+step7 = day + step6;
+step8 = step7;
+
+step9 = step8-1; //not a leap year 
+step10 = step9%7; 
+
+
