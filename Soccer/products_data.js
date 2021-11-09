acd76a101694567cf214af3fa9005fc4cca6a7f3
chloekamm <kamchloe11@gmail.com>
2021-11-08 22:39:41 -1000

    changes

--- Soccer/products_data.js
+++ Soccer/products_data.js
@@ -6,25 +6,25 @@
     var products = [
     { brand: "Soccer Ball",
      price: 24.99,
-     image: "soccerball.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/soccerball.jpeg?raw=true" },
 
      { brand: "Shin Guards",
      price: 19.99,
-     image: "shinguards.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/shinguards.jpeg?raw=true" },
 
      { brand: "Jersey",
      price: 65.99,
-     image: "jersey.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/jersey.jpeg?raw=true" },
 
      { brand: "Practice Cones",
      price: 18.99,
-     image: "cones.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/cones.jpeg?raw=true" },
 
      { brand: "Practice Net",
      price: 72.99,
-     image: "goal.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/goal.jpeg?raw=true" },
 
      { brand: "Soccer Cleats",
      price: 135.99 ,
-     image: "soccercleats.jpeg" },
+     image: "https://github.com/chloekamm/ITM352_F21/blob/main/Soccer/images/soccercleats.jpeg?raw=true" },
     ];
