dec202acf447ab5d2683c90f2d77c4f82da2e6d8
chloekamm <kamchloe11@gmail.com>
2021-11-08 21:58:42 -1000

    as1

---
+++ Soccer/products_data.js
@@ -0,0 +1,30 @@
+
+
+    
+    num_items=5;
+
+    var products = [
+    { brand: "Soccer Ball",
+     price: 24.99,
+     image: "soccerball.jpeg" },
+
+     { brand: "Shin Guards",
+     price: 19.99,
+     image: "shinguards.jpeg" },
+
+     { brand: "Jersey",
+     price: 65.99,
+     image: "jersey.jpeg" },
+
+     { brand: "Practice Cones",
+     price: 18.99,
+     image: "cones.jpeg" },
+
+     { brand: "Practice Net",
+     price: 72.99,
+     image: "goal.jpeg" },
+
+     { brand: "Soccer Cleats",
+     price: 135.99 ,
+     image: "soccercleats.jpeg" },
+    ];
