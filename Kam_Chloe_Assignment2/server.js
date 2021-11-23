03f61ff21f0add7d0fbbf145afebcefc66dc638b
chloekamm <kamchloe11@gmail.com>
2021-11-22 22:48:08 -0800

    updates

--- Kam_Chloe_Assignment2/server.js
+++ Kam_Chloe_Assignment2/server.js
@@ -8,13 +8,12 @@ var myParser = require("body-parser");
 var data = require('./public/products_data.js');
 var products = data.products;
 var filename = 'user_data.json';
-var fs = require('fs');
 const fs = require('fs');
 
 const { response } = require('express');
 
-
 const qs = require('querystring');
+const { request } = require('https');
 
 // Monitors all requests
 app.all('*', function (request, response, next) {
@@ -26,57 +25,93 @@ app.all('*', function (request, response, next) {
 //code from lab14 Ex3 
 
 var filename = 'user_data.json';
-if (fs.existsSync(filename)){
-    // have user_data file, so read data and parse into users_registration_info object
+if (fs.existsSync(filename)) {
+    // have user_data file, so read data and parse into users_reg_data object
     let user_data_str = fs.readFileSync(filename, 'utf-8'); // reads content of the file and returns as a string
-    var users_registration_info = JSON.parse(user_data_str); // parses into oject and stores in users_registration_info
+    var users_reg_data = JSON.parse(user_data_str); // parses into oject and stores in users_reg_data
     var file_stats = fs.statSync(filename);
     console.log(`${filename} has ${file_stats.size} characters`);
 } else {
     console.log(`Hey! ${filename} does not exist!`)
 }
 
-app.get("/", function (request,response) {
+app.get("/", function (request, response) {
     response.send("Nothing here!");
 });
-
 app.use(express.urlencoded({ extended: true })); // if you get a POST request from a URL it will put the request in the body so you can use the data
+
+
+//Code from lab 14 and Assingment 2 examples
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="register" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/register", function (request, response) {
+    username = request.body['username'];
+    // process a simple register form
+    if (typeof users_reg_data[username] == 'undefined' && (request.body['password'] == request.body['repeat_password'])) {
+        users_reg_data[username] = {};
+        users_reg_data[username].password = request.body['password'];
+        users_reg_data[username].email = request.body['email'];
+
+        fs.writeFileSync('./user_data.json', JSON.stringify(users_reg_data));
+        response.redirect('./login');
+        console.log("successfully registered");
+    } else {
+        response.redirect('./register');
+        console.log("not registered");
+    }
+});
 
 app.get("/login", function (request, response) {
     // Give a simple login form
     str = `
-    
 <body>
 <form action="" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="text" name="username" size="40" placeholder="enter username" 
+><br />
 <input type="password" name="password" size="40" placeholder="enter password"><br />
 <input type="submit" value="Submit" id="submit">
 </form>
 </body>
 `;
     response.send(str);
- });
+});
 
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
     let login_username = request.body['username'];
     let login_password = request.body['password'];
-    // checks passwaord if user name exissts
-    if (typeof users_registration_info[login_username] != 'undefined'){ // if user matches what we have
-        if(users_registration_info[login_username]['password'] == login_password) {
+    // check if username exists, then check password entered matches password stored
+    if (typeof users_reg_data[login_username] != 'undefined') { // if user matches what we have
+        if (users_reg_data[login_username]['password'] == login_password) {
             response.send(`${login_username} is logged in`);
         } else {
-            response.redirect(`./login?err=incorrect password for ${login_username} `); 
+            response.redirect(`./login?err=incorrect password for ${login_username} `);
         }
     } else {
         response.redirect(`./login?err=${login_username} does not exist`);
     }
 
-    response.send('Processing login' + JSON.stringify(request.body)) // request.body holds the username & password- forms data 
+    response.send('Processing login' + JSON.stringify(request.body)) // request.body holds the username & password (the form data when it got posted)
 
 });
 
+app.use(express.urlencoded({ extended: true })); // if you get a POST request from a URL it will put the request in the body so you can use the data
 
+    
 //Get request for products data
 app.get('/products.js', function (request, response) {
     response.type('.js');
