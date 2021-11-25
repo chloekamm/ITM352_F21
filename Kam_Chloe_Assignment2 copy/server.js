984c03eeaca0fa7ac813cd5f1936a1112da38426
chloekamm <kamchloe11@gmail.com>
2021-11-25 14:08:33 -0800

    updates

--- Kam_Chloe_Assignment2 copy/server.js
+++ Kam_Chloe_Assignment2 copy/server.js
@@ -30,9 +30,9 @@ if (fs.existsSync(filename)) {
     let user_data_str = fs.readFileSync(filename, 'utf-8'); // reads content of the file and returns as a string
     var users_reg_data = JSON.parse(user_data_str); // parses into oject and stores in users_reg_data
     var file_stats = fs.statSync(filename);
-    console.log(`${filename} has ${file_stats.size} characters`);
+    console.log(`${filename} has ${file_stats.size} characters`); //displayed in console 
 } else {
-    console.log(`Hey! ${filename} does not exist!`)
+    console.log(`Hey! ${filename} does not exist!`) //Displayed in console if file cannot be found 
 }
 
 app.get("/", function (request, response) {
@@ -58,17 +58,25 @@ app.use(express.urlencoded({ extended: true })); // if you get a POST request fr
     response.send(str);
 });
 */
+
+//Register process
 app.post("/register", function (request, response) {
     username = request.body['username'];
     // process a simple register form
+
+    //Checking if user input is valid- 
     if (typeof users_reg_data[username] == 'undefined' && (request.body['password'] == request.body['repeat_password'])) {
         users_reg_data[username] = {};
         users_reg_data[username].password = request.body['password'];
         users_reg_data[username].email = request.body['email'];
 
+        //If data is valid, data is loading into the file of user information
+        //If loaded correctly, user is directed to invoice page. 
         fs.writeFileSync('./user_data.json', JSON.stringify(users_reg_data));
-        response.redirect('./login.html');
+        response.redirect('./invoice.html?');
         console.log("successfully registered");
+
+        //If data is not valid, user is directed back to register page to correct errors 
     } else {
         response.redirect('./register.html');
         console.log("not registered");
@@ -95,20 +103,28 @@ app.post("/login", function (request, response) {
     let login_username = request.body['username'];
     let login_password = request.body['password'];
     // check if username exists, then check password entered matches password stored
-    var errors = {};
+    var errors = {}; //needed to validate data 
 
+    //So user is not case sensitive. 
+    //login_username = request.body.toLowerCase();  
     if (typeof users_reg_data[login_username] != 'undefined') { // if user matches what we have
         if (users_reg_data[login_username]['password'] == login_password) {
+            response.redirect('./invoice.html'+ queryString.stringify(req.query));
+            console.log(`successfully logged in`);
             response.send(`${login_username} is logged in`);
+             
         } else {
+            //errors['logerrors']=  `${login_password} is incorrect for ${login_username} `;
             response.redirect(`./login.html?err=incorrect password for ${login_username} `);
-            
+            console.log('password is incorrect, please try again');
         }
     } else {
         response.redirect(`./login.html?err=${login_username} does not exist`);
+        console.log('username does not exist, please register');
+        
     }
 
-    response.send('Processing login' + JSON.stringify(request.body)) // request.body holds the username & password (the form data when it got posted)
+    //response.send('Processing login' + JSON.stringify(request.body)) // request.body holds the username & password (the form data when it got posted)
 
 });
 //Get request for products data
@@ -126,7 +142,7 @@ function isNonNegInt(q, returnErrors = false) {
     if (q == '') q = 0;
     if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value. 
     else {
-        if(q>25) errors.push('Not enough in stock. '); //checks quanitity
+        if (q > 25) errors.push('Not enough in stock. '); //checks quanitity
         if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
 
         if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
@@ -136,42 +152,42 @@ function isNonNegInt(q, returnErrors = false) {
 }
 //From lab 13 
 //to access inputted data from products.js
-app.use(express.urlencoded ({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Get the quanitity data from the order form, then check it and if all good send it to the invoice, if not send the user back to purchase page
 app.post("/process_form", function (request, response) {
-    
+
     let POST = request.body;
 
     // check to make user inputs some value
 
-   // check is quantities are valid (nonnegint and have inventory)
-   var errors = {};
+    // check is quantities are valid (nonnegint and have inventory)
+    var errors = {};
 
 
-    for(i in request.body.quantity) {
-        if(!isNonNegInt(request.body.quantity[i])) {
+    for (i in request.body.quantity) {
+        if (!isNonNegInt(request.body.quantity[i])) {
             console.log(`${request.body.quantity[i]} is not a valid quantity for ${products[i].brand}`);
-            errors['quantity'+i] = `${request.body.quantity[i]} is not a valid quantity for ${products[i].brand}`;
-        
-        }
-        if((!isNonNegInt)>products[i].inventory){
-            errors['inventory'+i] = ` We do not have ${request.body.quantity[i]} products in stock for ${products[i].brand} sorry for inconvenience ` ;
-        
-        
-    }
-}
-    
-   let qty_obj = {"quantity": JSON.stringify(request.body.quantity)};
-   if(Object.keys(errors).length === 0) {
-      
+            errors['quantity' + i] = `${request.body.quantity[i]} is not a valid quantity for ${products[i].brand}`;
 
-    //If data is valid, create invoice
-    response.redirect('./invoice.html?' + qs.stringify(qty_obj));
-   } else {
-    qty_obj.errors = JSON.stringify(errors);
-    response.redirect('./products_display.html?' + qs.stringify(qty_obj));
-   }
+        }
+        if ((!isNonNegInt) > products[i].inventory) {
+            errors['inventory' + i] = ` We do not have ${request.body.quantity[i]} products in stock for ${products[i].brand} sorry for inconvenience `;
+
+
+        }
+    }
+
+    let qty_obj = { "quantity": JSON.stringify(request.body.quantity) };
+    if (Object.keys(errors).length === 0) {
+
+
+        //If data is valid, go to login to then direct user to invoice once logged in. 
+        response.redirect('./login.html?' + qs.stringify(qty_obj));
+    } else {
+        qty_obj.errors = JSON.stringify(errors);
+        response.redirect('./products_display.html?' + qs.stringify(qty_obj));
+    }
 });
 //route all other GEt requests to files in the public folder. 
 app.use(express.static('./public'));
