aa33c96cc22ef20a9af5434f7c96e4cd6a29227e
chloekamm <kamchloe11@gmail.com>
2021-12-13 09:20:20 -1000

    style changes

--- Kam_Chloe_Assignment3/server.js
+++ Kam_Chloe_Assignment3/server.js
@@ -23,18 +23,19 @@ var saved_user_quantity_array; // to tmp store user  selected quantities until n
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 //From examples from assignment 3-- Initalize session
-app.use(session({ secret: "MySecretKey", resave: true, saveUninitialized: true
+app.use(session({
+    secret: "MySecretKey", resave: true, saveUninitialized: true
 }
-)); 
+));
 
 // Monitors all requests
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to' + request.path);
-     // need to initialize an object to store the cart in the session. We do it when there is any request so that we don't have to check it exists
+    // need to initialize an object to store the cart in the session. We do it when there is any request so that we don't have to check it exists
     // anytime it's used
     if (typeof request.session.cart == 'undefined') { request.session.cart = {}; }
     next();
-   
+
 });
 
 // get the body - if you get a POST request from a URL it will put the request in the body so you can use the data
@@ -48,7 +49,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/get_cart", function (request, response) {
     response.json(request.session.cart);
-}); 
+});
 
 //Get request for products data
 app.get("/product_data.js", function (request, response, next) {
@@ -89,11 +90,11 @@ app.use(express.urlencoded({ extended: true })); // if you get a POST request fr
 app.post("/register", function (request, response) {
     username = request.body.username.toLowerCase(); //So username is not case sensitive
 
-  
+
     // process a simple register form
 
     //Checking if user input is valid- 
-    if(typeof users_reg_data[username] !='undefined'){
+    if (typeof users_reg_data[username] != 'undefined') {
         errors['username_taken'] = `Hey! ${username} is already registered!`;
     }
     if (typeof users_reg_data[username] == 'undefined' && (request.body['password'] == request.body['repeat_password'])) {
@@ -116,7 +117,7 @@ app.post("/register", function (request, response) {
 
         //If data is not valid, user is directed back to register page to correct errors 
     } else {
-        response.redirect('./register.html'+ params.toString());
+        response.redirect('./register.html' + params.toString());
         console.log("not registered");
     }
 });
@@ -124,59 +125,56 @@ app.post("/register", function (request, response) {
 
 app.post("/login", function (request, response) {
 
-//empty basket of errors
-var errors = {}; 
-//string for messages
-var loginMessage_str = '';
-var incorrectLogin_str = '';
-let params = new URLSearchParams(request.query);
-    
+    //empty basket of errors
+    var errors = {};
+    //string for messages
+    var loginMessage_str = '';
+    var incorrectLogin_str = '';
+    let params = new URLSearchParams(request.query);
+
     // Process login form POST and redirect to logged in page if ok, back to login page if not
     let login_username = request.body['username'].toLowerCase();  //So user is not case sensitive. 
     let login_password = request.body['password'];
     // check if username exists, then check password entered matches password stored
-    
+
     var log_errors = []; //start with no errors 
     if (typeof users_reg_data[login_username] != 'undefined') { // if user matches what we have
         if (users_reg_data[login_username]['password'] == login_password) {
-             // store username, email, and full name in the session
-             request.session['username'] = login_username;
-             request.session['email'] = users_reg_data[login_username]['email'];
-        //display name on products display
+            // store username, email, and full name in the session
+            request.session['username'] = login_username;
+            request.session['email'] = users_reg_data[login_username]['email'];
 
-        var user_info = {"username": login_username, "name": users_reg_data[login_username].name, "email": users_reg_data[login_username].email};
-        response.cookie('user_info', JSON.stringify(user_info), { maxAge: 30 * 60 * 1000 }); //Gives cookie and stores user data 
+            //display name on products display
+            //Gets variable for cookie to display username on pages
+            var user_info = { "username": login_username, "name": users_reg_data[login_username].name, "email": users_reg_data[login_username].email };
+            //Gives cookie a expiration time, it will hold user data for 30 minutes. 
+            response.cookie('user_info', JSON.stringify(user_info), { maxAge: 30 * 60 * 1000 });
 
-             
+
+
             console.log(`Welcome ${request.session['username']}`);
-            // Create a username cookie with expiration time
-        
-            // create username cookie 
-           
-            // create username cookie 
-           // response.cookie('username', login_username);
-            //console.log(request.cookies);
 
 
-           /* Used to store user data to display user info in assignment 2 
-           let params = new URLSearchParams();
-            params.append('quantity',JSON.stringify( saved_user_quantity_array));
-            params.append('username',login_username);
-            params.append('email',users_reg_data[login_username]['email']);
-            */
-          
-           //Need to figure out a way to display username to know user is logged in. 
+
+            /* Used to store user data to display user info in assignment 2 
+            let params = new URLSearchParams();
+             params.append('quantity',JSON.stringify( saved_user_quantity_array));
+             params.append('username',login_username);
+             params.append('email',users_reg_data[login_username]['email']);
+             */
+
+            //Need to figure out a way to display username to know user is logged in. 
             //console.log(`${request.cookies} is logged in`); 
 
-             //display name on products display
-           
+            //display name on products display
+
             console.log(`${login_username} successfully logged in`);
-           
-            response.redirect('./products_display.html?products_key=Soccer%20Cleats' );
-          
-           
-          return;
-             
+
+            response.redirect('./products_display.html?products_key=Soccer%20Cleats');
+
+
+            return;
+
         } else {
             //Code examples from Tina Vo and W3schools 
             incorrectLogin_str = 'The password is incorrect, please try again!';
@@ -187,11 +185,11 @@ let params = new URLSearchParams(request.query);
 
 
     } else {
-     
-       incorrectLogin_str = 'The username does not exists or invalid. Please try again or register!';
-       console.log(errors);
-       request.query.login_username = login_username;
-   
+
+        incorrectLogin_str = 'The username does not exists or invalid. Please try again or register!';
+        console.log(errors);
+        request.query.login_username = login_username;
+
     }
     //make username sticky when there's an error
     //SHows error message in browser in addition to showing using with an alert. 
@@ -229,7 +227,7 @@ app.post("/add_to_cart", function (request, response, next) {
     // Validations 
     var errors = {}; //assume no errors to start
     var empty = true // assume no quantities entered
- 
+
 
     for (let i in products_data[products_key]) {
         q = POST['quantity' + i];
@@ -250,20 +248,20 @@ app.post("/add_to_cart", function (request, response, next) {
             console.log("Please enter some quantities.");
         }
     }
-  //Remove items purchased from qunatity available Code from Assignment 2;
- /* NEED TO UPDATE 
-  for(let i in saved_user_quantity_array) {
-    products[i].quantity_available -= Number(saved_user_quantity_array[i]);
-}
-
-*/
+    //Remove items purchased from qunatity available Code from Assignment 2;
+    /* NEED TO UPDATE 
+     for(let i in saved_user_quantity_array) {
+       products[i].quantity_available -= Number(saved_user_quantity_array[i]);
+   }
+   
+   */
     // if there are errors, display each error on a new line
     if (Object.keys(errors).length > 0) {
         var errorMessage_str = '';
         for (err in errors) {
             errorMessage_str += errors[err] + '\n';
         }
-        
+
         let params = new URLSearchParams(request.body);
         params.append('errorMessage', errorMessage_str);
         response.redirect(`./products_display.html?${params.toString()}`);
@@ -280,17 +278,17 @@ app.post("/add_to_cart", function (request, response, next) {
             // if the i item in products_key already exists in the cart, add quantities_requested to the existing value
             if (typeof request.session.cart[products_key][i] != 'undefined') {
                 request.session.cart[products_key][i] += quantity_requested;
-            // else if the i item in products_key doesn't exist in the cart, add quantity_requested 
+                // else if the i item in products_key doesn't exist in the cart, add quantity_requested 
             } else {
                 request.session.cart[products_key][i] = quantity_requested;
             }
         }
         //Show session in console 
         console.log(request.session.cart);
-   
+
     }
     let params = new URLSearchParams(request.body);
-  //  params.append('products_key', products_key); 
+    //  params.append('products_key', products_key); 
     response.redirect(`./products_display.html?${params.toString()}`);
 });
 
